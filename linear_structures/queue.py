109a6fab7db0848296f797b68cef93cb6a6c24d2
Yuval <yvw.bor@gmail.com>
2014-08-13 21:25:12 +0300

    Reimplement using deque for O1 removal

--- linear_structures/queue.py
+++ linear_structures/queue.py
@@ -1,26 +1,33 @@
+from collections import deque
+
 class Queue(object):
     '''
-        A basic implementation of a queue using lists
-        O(1) addition, O(n) removal
+        A basic implementation of a queue using a Deque
+        O(1) addition and removal
+
+        enqueue(self, item) => Add an item to back of the queue
+        dequeue(self, item) => Remove an item from the front of the queue, raise IndexError if empty
+        is_empty(self) => return True if empty
+        __len__(self) => return the number of elements in the queue
     '''
 
     def __init__(self):
-        self.list = []
+        self.items = deque()
 
     def __str__(self):
-        return str(self.list)
+        return str(self.items)
 
     def enqueue(self, item):
-        self.list.append(item)
+        self.items.append(item)
 
     def dequeue(self):
-        return self.list.pop(0)
+        return self.items.popleft()
 
     def is_empty(self):
-        return self.list == []
+        return len(self.items) == 0
 
-    def size(self):
-        return len(self.list)
+    def __len__(self):
+        return len(self.items)
 
 if __name__ == '__main__':
     a = Queue()
@@ -30,7 +37,7 @@ if __name__ == '__main__':
     a.enqueue('d')
     print(a.dequeue())
     print(a.dequeue())
-    print(a.size(), a.is_empty())
+    print(len(a), a.is_empty())
     print(a.dequeue())
     print(a.dequeue())
     print (a.is_empty())
